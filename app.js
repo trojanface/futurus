3f48d8cb0b833be9601d374e30e5a73e28c4f402
MattT <trojanface@hotmail.com>
2020-08-01 13:33:40 +0930

    Initial deployment commit 6

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
